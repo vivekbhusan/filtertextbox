3eda0071ec3137b2285df12443b7850c016c443a
vivekbhusan <vivek@mymarketingmatters.com>
2025-02-20 01:24:18 +0530

    Added new page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react"
 
-function App() {
+const App = ()=> {
+
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
+  const [searchData, setSearchData] = useState("");
+  const apiUrl="https://jsonplaceholder.typicode.com/users";
+  useEffect(() =>{
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json)=> {
+      setData(json);
+      setFilterData(json);
+    });
+  },[]);
+
+  function FilteredData(text){
+    if(!text){
+      setFilterData(data); //Reset if Search is empty
+      return;
+    }
+    const fullFilterData = data.filter((item)=>
+      item.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilterData(fullFilterData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input type="text" onChange={(event) => setSearchData(event.target.value)} />
+        <button onClick={() => FilteredData(searchData)}>Search</button>
+        <ul>
+        {
+          filterData.map((dat)=>(
+            <li key ={dat.id}>{dat.name}</li>
+           ))
+        }
+        </ul>
     </div>
   );
 }
